bfbd149f681879e76d88bd8548965afcb0b0684f
VolodymyrK2 <84743209+VolodymyrK2@users.noreply.github.com>
2023-03-21 16:12:21 +0000

    Deploying to gh-pages from @ VolodymyrK2/goit-js-hw-09@b4e0a6935b225aeef2a7c24f8071264511a3ed0c 🚀

---
+++ 01-color-switcher.38ebd843.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(()=>{e.setAttribute("disabled",""),o||(o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),r.addEventListener("click",(()=>{e.removeAttribute("disabled"),clearInterval(o),o=null}))}();
+//# sourceMappingURL=01-color-switcher.38ebd843.js.map
